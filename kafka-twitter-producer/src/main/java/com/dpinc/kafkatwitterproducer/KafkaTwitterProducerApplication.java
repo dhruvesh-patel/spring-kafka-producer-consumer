83bed4e72d2f3d44af6e9256795d81ac0749ce7c
Dhruvesh Patel <dmpatel.1412@gmail.com>
2020-08-25 22:18:58 +0100

    Spring boot application for kafka producer

---
+++ kafka-twitter-producer/src/main/java/com/dpinc/kafkatwitterproducer/KafkaTwitterProducerApplication.java
@@ -0,0 +1,13 @@
+package com.dpinc.kafkatwitterproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTwitterProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTwitterProducerApplication.class, args);
+	}
+
+}
